4424aa8bbd4312c10ae8d68ae4254cc95dc12d1f
Brian Sherson <brian.sherson@gmail.com>
2020-07-26 10:34:33 -0700

    Now how did I forget this?

---
+++ transcode/__init__.py
@@ -0,0 +1,19 @@
+from . import containers
+from . import filters
+from . import encoders
+from . import config
+import os
+
+def open(file, mode="r"):
+    stem, ext = os.path.splitext(file)
+
+    if mode == "r":
+        reader = containers.readers_by_ext[ext.lower()]
+        return reader[0](file)
+
+    elif mode == "w":
+        writer = containers.writers_by_ext[ext.lower()]
+        return writer[0](file)
+
+    else:
+        raise ValueError(f"Unknown mode: {mode}")
