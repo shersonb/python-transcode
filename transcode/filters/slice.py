93d3e18f2626a1c291b67c5accb7ef89e0da68f0
Brian Sherson <brian.sherson@gmail.com>
2020-07-26 10:33:27 -0700

    Adding firstframekey attribute to Slice, adding precision behavior.

--- transcode/filters/slice.py
+++ transcode/filters/slice.py
@@ -7,9 +7,10 @@ from ..util import cached
 from ..avarrays import toNDArray, toAFrame
 
 class Slice(BaseVideoFilter, BaseAudioFilter):
-    def __init__(self, startpts=0, endpts=None, **kwargs):
+    def __init__(self, startpts=0, endpts=None, firstframekey=False, **kwargs):
         self.startpts = startpts
         self.endpts = endpts
+        self.firstframekey = bool(firstframekey)
         super().__init__(**kwargs)
 
     @cached
@@ -41,16 +42,6 @@ class Slice(BaseVideoFilter, BaseAudioFilter):
     @cached
     def durations(self):
         return self.prev.durations[self.prev_start:self.prev_end]
-
-    @property
-    def layout(self):
-        if self.prev is not None:
-            return self.prev.layout
-
-    @property
-    def rate(self):
-        if self.prev is not None:
-            return self.prev.rate
 
     @property
     def time_base(self):
@@ -88,6 +79,10 @@ class Slice(BaseVideoFilter, BaseAudioFilter):
     def prev(self, value):
         self._prev = value
         self.reset_cache()
+
+    @property
+    def rate(self):
+        return self.prev.rate
 
     #def QTableColumns(self):
         #cols = []
@@ -152,6 +147,10 @@ class Slice(BaseVideoFilter, BaseAudioFilter):
 
             for n, frame in zip(N, I):
                 frame.pts = pts[n]
+
+                if n == 0 and self.firstframekey:
+                    frame.pict_type = "I"
+
                 yield frame
 
         elif self.type == "audio":
@@ -173,6 +172,7 @@ class Slice(BaseVideoFilter, BaseAudioFilter):
                 else:
                     end = self.prev.duration/self.prev.time_base
 
+                I = self.prev.iterFrames(start - 0.001/self.prev.time_base, end, whence)
 
             elif whence == "seconds":
                 start = self.startpts + max(start, 0)
@@ -189,17 +189,16 @@ class Slice(BaseVideoFilter, BaseAudioFilter):
                 else:
                     end = self.prev.duration
 
-
-            I = self.prev.iterFrames(start, end, whence)
+                I = self.prev.iterFrames(start - 0.001, end, whence)
 
             for frame in I:
                 if whence == "seconds":
-                    n1 = int(numpy.floor((start - frame.pts*frame.time_base)*frame.rate).clip(min=0))
-                    n2 = int(numpy.floor((frame.pts*frame.time_base + frame.samples/frame.rate - end)*frame.rate).clip(min=0))
+                    n1 = int(max(numpy.floor((start - frame.pts*frame.time_base)*frame.rate + 0.0001), 0))
+                    n2 = int(max(numpy.floor((frame.pts*frame.time_base + frame.samples/frame.rate - end)*frame.rate + 0.0001), 0))
 
                 elif whence == "pts":
-                    n1 = int(numpy.floor((start*frame.time_base - frame.pts*frame.time_base)*frame.rate).clip(min=0))
-                    n2 = int(numpy.floor((frame.pts*frame.time_base + frame.samples/frame.rate - end*frame.time_base)*frame.rate).clip(min=0))
+                    n1 = int(max(numpy.floor((start*frame.time_base - frame.pts*frame.time_base)*frame.rate + 0.0001), 0))
+                    n2 = int(max(numpy.floor((frame.pts*frame.time_base + frame.samples/frame.rate - end*frame.time_base)*frame.rate + 0.0001), 0))
 
                 if n1 or n2:
                     pts = frame.pts
@@ -212,6 +211,7 @@ class Slice(BaseVideoFilter, BaseAudioFilter):
 
                     frame = toAFrame(A, layout=frame.layout.name)
                     frame.rate = r
+                    frame.time_base = tb
 
                     if whence == "seconds":
                         frame.pts = max(pts - int(self.startpts/tb + 0.001), int((start - self.startpts)/tb + 0.001))
@@ -225,7 +225,36 @@ class Slice(BaseVideoFilter, BaseAudioFilter):
 
                 yield frame
 
+    def iterPackets(self, start=0, end=None, whence="pts"):
+        if whence == "seconds":
+            packets = self.prev.iterPackets(self.startpts + start, whence)
 
+        elif whence == "pts":
+            packets = self.prev.iterPackets(int(self.startpts/self.prev.time_base + 0.5) + start, whence)
+
+        for packet in packets:
+            if whence == "pts":
+                if end is not None and self.endpts is not None and packet.pts*packet.time_base >= min(self.endpts, self.startpts + end*packet.time_base):
+                    break
+
+                elif self.endpts is not None and packet.pts*packet.time_base >= self.endpts:
+                    break
+
+                elif end is not None and packet.pts*packet.time_base >= self.startpts + end*packet.time_base:
+                    break
+
+            elif whence == "seconds":
+                if end is not None and self.endpts is not None and packet.pts*packet.time_base >= min(self.endpts, self.startpts + end):
+                    break
+
+                elif self.endpts is not None and packet.pts*packet.time_base >= self.endpts:
+                    break
+
+                elif end is not None and packet.pts*packet.time_base >= self.startpts + end:
+                    break
+
+            packet.pts -= int(self.startpts/self.time_base + 0.5)
+            yield packet
 
     def __reduce__(self):
         return type(self), (), self.__getstate__()
@@ -238,9 +267,14 @@ class Slice(BaseVideoFilter, BaseAudioFilter):
 
         state["startpts"] = self.startpts
         state["endpts"] = self.endpts
+
+        if self.firstframekey:
+            state["firstframekey"] = True
+
         return state
 
     def __setstate__(self, state):
         self.prev = state.get("prev")
         self.startpts = state.get("startpts", 0)
         self.endpts = state.get("endpts")
+        self.firstframekey = bool(state.get("firstframekey", False))
