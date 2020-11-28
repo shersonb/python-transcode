729b79d52c46e7dbdd530a25e6ed44c6b1774b3e
Brian Sherson <brian.sherson@gmail.com>
2020-11-27 18:50:16 -0800

    Adding fps filter.

---
+++ transcode/filters/video/fps/__init__.py
@@ -0,0 +1,59 @@
+from ..base import BaseVideoFilter
+from fractions import Fraction as QQ
+from numpy import arange
+from transcode.util import cached
+
+
+class Fps(BaseVideoFilter):
+    """Change Frame Rate."""
+
+    allowedtypes = ("video",)
+
+    def __init__(self, rate=QQ(24000, 1001),
+                 prev=None, next=None, parent=None):
+        super().__init__(prev=prev, next=next, parent=parent)
+        self.rate = rate
+
+    def __getstate__(self):
+        state = super().__getstate__()
+        state["rate"] = self.rate
+        return state
+
+    def __setstate__(self, state):
+        self.rate = state.get("rate", QQ(24000, 1001))
+        super().__setstate__(state)
+
+    def __str__(self):
+        if self is None:
+            return "Fps"
+        return f"Fps({self.rate})"
+
+    @property
+    def rate(self):
+        return self._rate
+
+    @rate.setter
+    def rate(self, value):
+        self._rate = value
+        self.reset_cache()
+
+    @cached
+    def pts_time(self):
+        return arange(0, self.prev.framecount/float(self.rate), 1/float(self.rate))
+
+    @cached
+    def duration(self):
+        return self.framecount/float(self.rate)
+
+    def _processFrames(self, iterable):
+        for frame in iterable:
+            index = self.prev.frameIndexFromPts(frame.pts)
+            frame.pts = self.pts[index]
+            yield frame
+
+    @staticmethod
+    def QtDlgClass():
+        from .qfps import FpsDlg
+        return FpsDlg
+
+
---
+++ transcode/filters/video/fps/qfps.py
@@ -0,0 +1,72 @@
+from PyQt5.QtCore import Qt, pyqtSlot, QRegExp, QTime
+from PyQt5.QtGui import QRegExpValidator, QPen, QColor
+from PyQt5.QtWidgets import (QAction, QLabel, QSpinBox, QGridLayout, QVBoxLayout,
+                             QHBoxLayout, QLineEdit, QScrollArea, QWidget, QComboBox)
+
+from functools import partial
+from fractions import Fraction as QQ
+from transcode.pyqtgui.qfilterconfig import QFilterConfig
+from . import Fps
+import regex
+
+
+class FpsDlg(QFilterConfig):
+    def _createControls(self):
+        self.setWindowTitle("Configure Crop")
+
+        layout = QVBoxLayout(self)
+        self.setLayout(layout)
+
+        self.sourceWidget = QWidget(self)
+        self.sourceSelection = self.createSourceControl(self.sourceWidget)
+        self.sourceSelection.currentDataChanged.connect(self.setFilterSource)
+
+        srclayout = QHBoxLayout()
+        srclayout.addWidget(QLabel("Source: ", self.sourceWidget))
+        srclayout.addWidget(self.sourceSelection)
+
+        self.sourceWidget.setLayout(srclayout)
+        layout.addWidget(self.sourceWidget)
+
+        regex = QRegExp(r"^(\d+(?:\.\d+)?|\.\d+|\d+/\d+)$")
+        validator = QRegExpValidator(regex)
+
+        fpsLabel = QLabel("Frame rate:", self)
+        self.fpsEdit = QLineEdit(self)
+        self.fpsEdit.setText("24000/1001")
+        self.fpsEdit.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        self.fpsEdit.setValidator(validator)
+        self.fpsEdit.textChanged.connect(self.handleRateChanged)
+
+        hlayout = QHBoxLayout()
+        hlayout.addWidget(fpsLabel)
+        hlayout.addWidget(self.fpsEdit)
+
+        layout.addLayout(hlayout)
+
+        self._prepareDlgButtons()
+
+    def createNewFilterInstance(self):
+        return Fps()
+
+    @pyqtSlot(str)
+    def handleRateChanged(self, value):
+        if not regex.match(r"^(\d+(?:\.\d+)?|\.\d+|\d+/\d+)$", value):
+            return
+
+        if regex.match(r"^\d+/\d+$", value):
+            value = QQ(value)
+
+        elif regex.match(r"^\d+$", sar):
+            value = int(value)
+
+        else:
+            value = float(value)
+
+        self.shadow.rate = value
+        self.isModified()
+
+    def _resetControls(self):
+        self.fpsEdit.blockSignals(True)
+        self.fpsEdit.setText(f"{self.shadow.rate}")
+        self.fpsEdit.blockSignals(False)
