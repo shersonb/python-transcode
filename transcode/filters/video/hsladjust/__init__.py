91399b68993fd4680ebd852a04b1aea9fc75523f
Brian Sherson <brian.sherson@gmail.com>
2020-12-16 14:00:44 -0800

    Add HSL Adjustment filter, update setup.py.

---
+++ transcode/filters/video/hsladjust/__init__.py
@@ -0,0 +1,132 @@
+from ..base import BaseVideoFilter
+from ...base import CacheResettingProperty
+from fractions import Fraction as QQ
+from numpy import arange, moveaxis, float, zeros, uint8, float64, array
+from numpy import min as npmin
+from transcode.util import cached
+from transcode.avarrays import toNDArray, toVFrame
+
+
+class HSLAdjust(BaseVideoFilter):
+    """Adjust Hue/Saturation/Luminosity."""
+
+    allowedtypes = ("video",)
+
+    def __init__(self, dh=0, sfactor=1, lgamma=1,
+                 prev=None, next=None, parent=None):
+        super().__init__(prev=prev, next=next, parent=parent)
+        self.dh = dh
+        self.sfactor = sfactor
+        self.lgamma = lgamma
+
+    def __getstate__(self):
+        state = super().__getstate__()
+        state["dh"] = self.dh
+        state["sfactor"] = self.sfactor
+        state["lgamma"] = self.lgamma
+        return state
+
+    def __setstate__(self, state):
+        self.dh = state.get("dh", 0)
+        self.sfactor = state.get("sfactor", 1)
+        self.lgamma = state.get("lgamma", 1)
+        super().__setstate__(state)
+
+    def __str__(self):
+        if self is None:
+            return "HSLAdjust"
+        return f"HSLAdjust({self.dh}, {self.sfactor}, {self.lgamma})"
+
+    def _processFrames(self, iterable):
+        for frame in iterable:
+            if frame.format.name != "rgb24":
+                frame = frame.to_rgb()
+
+            A = toNDArray(frame)/256
+            R, G, B = moveaxis(A, 2, 0)
+            V = A.max(axis=2)
+            C = V - A.min(axis=2)
+            L = V - C/2
+
+            H = zeros(A.shape[:2], dtype=float64)
+
+            case1 = C == 0
+            case2 = (V == R)*(~case1)
+            case3 = (V == G)*(~case1)*(~case2)
+            case4 = (V == B)*(~case1)*(~case2)*(~case3)
+
+            H[case2] = (60*(G[case2] - B[case2])/C[case2]) % 360
+            H[case3] = 60*(2 + (B[case3] - R[case3])/C[case3])
+            H[case4] = 60*(4 + (R[case4] - G[case4])/C[case4])
+
+            SL = zeros(A.shape[:2], dtype=float64)
+
+            case5 = (L > 0)*(L < 1)
+            SL[case5] = (V[case5] - L[case5])/npmin((L[case5], 1-L[case5]), axis=0)
+
+            # --- Adjustments to HSL go here ---
+
+            H += self.dh
+            H %= 360
+
+            SL *= self.sfactor
+
+            L = 1 - (1 - L)**self.lgamma
+
+            C = (1 - abs(2*L - 1))*SL
+
+            H /= 60
+            X = C*(1 - abs(H % 2 - 1))
+
+            case1 = (H <= 1)
+            case2 = (1 < H)*(H <= 2)
+            case3 = (2 < H)*(H <= 3)
+            case4 = (3 < H)*(H <= 4)
+            case5 = (4 < H)*(H <= 5)
+            case6 = H > 5
+
+            m = L - C/2
+
+            R = zeros(R.shape, dtype=float64)
+            G = zeros(G.shape, dtype=float64)
+            B = zeros(B.shape, dtype=float64)
+
+            R[case1] = C[case1]
+            G[case1] = X[case1]
+
+            R[case2] = X[case2]
+            G[case2] = C[case2]
+
+            G[case3] = C[case3]
+            B[case3] = X[case3]
+
+            G[case4] = X[case4]
+            B[case4] = C[case4]
+
+            B[case5] = C[case5]
+            R[case5] = X[case5]
+
+            B[case6] = X[case6]
+            R[case6] = C[case6]
+
+            R += m
+            G += m
+            B += m
+
+            A = (256*moveaxis((R, G, B), 0, 2)).clip(min=0, max=255)
+            A = uint8(A)
+
+            newframe = toVFrame(A, frame.format.name)
+
+            newframe.time_base = frame.time_base
+            newframe.pts = frame.pts
+            newframe.pict_type = frame.pict_type
+
+            yield newframe
+
+    @staticmethod
+    def QtDlgClass():
+        from .qhsladjust import QHSLAdjDlg
+        return QHSLAdjDlg
+
+
---
+++ transcode/filters/video/hsladjust/qhsladjust.py
@@ -0,0 +1,130 @@
+from PyQt5.QtCore import Qt, pyqtSlot, QRegExp, QTime
+from PyQt5.QtGui import QRegExpValidator, QPen, QColor
+from PyQt5.QtWidgets import (QAction, QLabel, QSpinBox, QDoubleSpinBox, QGridLayout, QVBoxLayout,
+                             QHBoxLayout, QLineEdit, QScrollArea, QWidget, QComboBox)
+
+from functools import partial
+from fractions import Fraction as QQ
+from transcode.pyqtgui.qfilterconfig import QFilterConfig
+from transcode.pyqtgui.qframeselect import QFrameSelect
+from transcode.pyqtgui.qimageview import QImageView
+from . import HSLAdjust
+import regex
+
+
+class QHSLAdjDlg(QFilterConfig):
+    allowedtypes = ("video",)
+
+    def _createControls(self):
+        self.setWindowTitle("Configure Hue/Saturation/Luminosity")
+
+        layout = QVBoxLayout(self)
+        self.setLayout(layout)
+
+        self.sourceWidget = QWidget(self)
+        self.sourceSelection = self.createSourceControl(self.sourceWidget)
+        self.sourceSelection.currentDataChanged.connect(self.setFilterSource)
+
+        srclayout = QHBoxLayout()
+        srclayout.addWidget(QLabel("Source: ", self.sourceWidget))
+        srclayout.addWidget(self.sourceSelection)
+
+        self.sourceWidget.setLayout(srclayout)
+        layout.addWidget(self.sourceWidget)
+
+        self.imageView = QImageView(self)
+        layout.addWidget(self.imageView)
+
+        self.slider = QFrameSelect(self)
+        self.slider.frameSelectionChanged.connect(self.loadFrame)
+        layout.addWidget(self.slider)
+
+        hueLabel = QLabel("Hue adjustment:", self)
+        self.hueSpinBox = QSpinBox(self)
+        self.hueSpinBox.setMinimum(-179)
+        self.hueSpinBox.setMaximum(180)
+        self.hueSpinBox.valueChanged.connect(self._handleHueSpinBoxValueChanged)
+
+        satLabel = QLabel("Saturation factor:", self)
+        self.satSpinBox = QDoubleSpinBox(self)
+        self.satSpinBox.setDecimals(2)
+        self.satSpinBox.setSingleStep(0.1)
+        self.satSpinBox.setMinimum(0)
+        self.satSpinBox.setMaximum(10)
+        self.satSpinBox.valueChanged.connect(self._handleSatSpinBoxValueChanged)
+
+        lumLabel = QLabel("Luminosity factor:", self)
+        self.lumSpinBox = QDoubleSpinBox(self)
+        self.lumSpinBox.setDecimals(2)
+        self.lumSpinBox.setMinimum(0)
+        self.lumSpinBox.setMaximum(10)
+        self.lumSpinBox.valueChanged.connect(self._handleLumSpinBoxValueChanged)
+
+        hlayout = QHBoxLayout()
+
+        hlayout.addStretch()
+        hlayout.addWidget(hueLabel)
+        hlayout.addWidget(self.hueSpinBox)
+
+        hlayout.addStretch()
+        hlayout.addWidget(satLabel)
+        hlayout.addWidget(self.satSpinBox)
+
+        hlayout.addStretch()
+        hlayout.addWidget(lumLabel)
+        hlayout.addWidget(self.lumSpinBox)
+
+        hlayout.addStretch()
+
+        layout.addLayout(hlayout)
+
+        self._prepareDlgButtons()
+
+    def createNewFilterInstance(self):
+        return HSLAdjust()
+
+    def _resetControls(self):
+        self.hueSpinBox.blockSignals(True)
+        self.hueSpinBox.setValue(self.filtercopy.dh)
+        self.hueSpinBox.blockSignals(False)
+
+        self.satSpinBox.blockSignals(True)
+        self.satSpinBox.setValue(self.filtercopy.sfactor)
+        self.satSpinBox.blockSignals(False)
+
+        self.lumSpinBox.blockSignals(True)
+        self.lumSpinBox.setValue(self.filtercopy.lgamma)
+        self.lumSpinBox.blockSignals(False)
+
+        if self.filtercopy.prev is not None:
+            self.slider.setPtsTimeArray(self.filtercopy.prev.pts_time)
+            self.loadFrame(self.slider.slider.value(), QTime())
+
+    def _handleHueSpinBoxValueChanged(self, value):
+        self.filtercopy.dh = value
+        self.isModified()
+        self.loadFrame(self.slider.slider.value(), None)
+
+    def _handleSatSpinBoxValueChanged(self, value):
+        self.filtercopy.sfactor = value
+        self.isModified()
+        self.loadFrame(self.slider.slider.value(), None)
+
+    def _handleLumSpinBoxValueChanged(self, value):
+        self.filtercopy.sfactor = value
+        self.isModified()
+        self.loadFrame(self.slider.slider.value(), None)
+
+    @pyqtSlot(int, QTime)
+    def loadFrame(self, n, t):
+        if self.filtercopy.prev is not None:
+            frame = next(self.filtercopy.iterFrames(n, whence="framenumber"))
+            im = frame.to_image()
+            pixmap = im.toqpixmap()
+            self.imageView.setFrame(pixmap)
+            #self.imageView.resize(pixmap.size())
+
+    def _prevChanged(self, source):
+        self.slider.setPtsTimeArray(source.pts_time)
+        self.loadFrame(self.slider.slider.value(),
+                       self.slider.currentTime.time())
