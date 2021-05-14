b53abc024cf3cd09ef94bef362f02b8538d82256
Brian Sherson <brian.sherson@gmail.com>
2021-05-14 14:40:38 -0700

    Cleanup, error-proof attribute.

--- transcode/filters/audio/base.py
+++ transcode/filters/audio/base.py
@@ -30,19 +30,7 @@ class BaseAudioFilter(BaseFilter):
         if self.prev is not None and self.prev.type == "audio":
             return self.prev.bitdepth
 
-    #@property
-    #def prev(self):
-        #return self._prev or self._source
-
-    #@prev.setter
-    #def prev(self, value):
-        #if value is not None and value.type != "audio":
-            #raise ValueError(
-                #f"Invalid media source for Audio filter: {value.type}")
-
-        #self._prev = value
-        #self.reset_cache()
-
     @cached
     def duration(self):
-        return self.prev.duration
+        if self.prev is not None:
+            return self.prev.duration
